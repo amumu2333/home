3f6d352b32abdbcc2858b041e7a6aa5c7ed17ea1
付军军 <1045245697@qq.com>
2017-03-31 08:32:35 +0800

    change

--- js/index.js
+++ js/index.js
@@ -1,100 +1,140 @@
-$(window).ready(function () {
 
+
+//进入动画
 (function () {
-  var now = 0;
-  var part = $(".view .part");
-  $(".nav_box a").each(function (i) {
-      $(this).on("click",function () {
-        console.log(now);
-        if (i == now) {
-          return;
-        }
-        part
-        .eq(i)
-          .css("transition","")
-          .addClass("active")
-        .siblings()
-          .removeClass("active");
-        now = i;
-      })
+  var list=document.querySelector('.boxList');
+  $(window).ready(function () {
+    setTimeout(transition,1000);
   });
-
-  var h = document.documentElement.clientHeight;
-  $(".part .inner .item").css("height",h+"px");
-
-  var i=0;
-  var flag = true;
-
-  $(".part").on("mousewheel",function (event, delta, deltaX, deltaY) {
-    var top = parseInt($(".inner",this).css("margin-top"));
-    var bottom = parseInt($(".inner",this).css("margin-bottom"));
-    var max = $(".inner .item").length;
-    if (flag) {
-      flag = !flag;
-      switch (delta) {
-        case 1 :
-          //xia
-          top+=h;
-          if (top>0) {
-            top = 0;
-          }
-          $(".inner",this).css("margin-top",top+"px");
-
-          break;
-        case -1 :
-          //shang
-          top-=h;
-          if (top<-h*(max-1)) {
-            top = -h*(max-1);
-          }
-          $(".inner",this).css("margin-top",top+"px");
-          break;
-        default:
-
-      }
+  function transition(){
+    list.className='on boxList';
+    $(".boxList img").addClass("active")
+  }
+})();
 
 
-
-
-
-
-
-
-
-
-      setTimeout(function () {
-        flag = !flag
-      },1000)
-    }else {
-      return
+// 线条粒子  背景
+!function() {
+    function A(a, b, c) {
+        return a.getAttribute(b) || c
     }
+    function F(a) {
+        return document.getElementsByTagName(a);
+    }
+    function D() {
+        var c = F("script"),
+            a = c.length,
+            b = c[a - 1];
+        return {
+            l: a,
+            z: A(b, "zIndex", -1),
+            o: A(b, "opacity", 1),
+            c: A(b, "color", "255,255,255"),
+            n: A(b, "count", 199)
+        }
+    }
+    function E() {
+        x = i.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        B = i.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    }
+    function M() {
+        J.clearRect(0, 0, x, B);
+        var c = [I].concat(v);
+        var b, d, a, g, e, f;
+        v.forEach(function(h) {
+            h.x += h.xa,
+                h.y += h.ya,
+                h.xa *= h.x > x || h.x < 0 ? -1 : 1,
+                h.ya *= h.y > B || h.y < 0 ? -1 : 1,
+                J.fillRect(h.x - 0.5, h.y - 0.5, 1, 1);
+            for (d = 0; d < c.length; d++) {
+                b = c[d];
+                if (h !== b && null !== b.x && null !== b.y) {
+                    g = h.x - b.x;
+                    e = h.y - b.y;
+                    f = g * g + e * e;
+                    f < b.max && (b === I && f >= b.max / 2 && (h.x -= 0.03 * g, h.y -= 0.03 * e), a = (b.max - f) / b.max, J.beginPath(), J.lineWidth = a / 2, J.strokeStyle = "rgba(" + w.c + "," + (a + 0.2) + ")", J.moveTo(h.x, h.y), J.lineTo(b.x, b.y), J.stroke())
+                }
+            }
+            c.splice(c.indexOf(h), 1);
+        }),
+            C(M);
+    }
+    var i = document.createElement("canvas"),
+        w = D(),
+        L = "c_n" + w.l, //c_n2
+        J = i.getContext("2d"),
+        x,
+        B,
+        C = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+            function(a) {
+                window.setTimeout(a, 1000 / 45)
+            },
+        N = Math.random,
+        I = {
+            x: null,
+            y: null,
+            max: 20000
+        };
+    i.id = L;
+    i.style.cssText = "position:fixed;top:0;left:0;z-index:" + w.z + ";opacity:" + w.o;
+    F("body")[0].appendChild(i);
+    E();
+    window.onresize = E;
+    window.onmousemove = function(a) {
+        a = a || window.event,
+            I.x = a.clientX,
+            I.y = a.clientY
+    };
+    window.onmouseout = function() {
+        I.x = null,
+            I.y = null
+    };
+    for (var v = [], z = 0; w.n > z; z++) {
+        var G = N() * x,
+            H = N() * B,
+            y = 2 * N() - 1,
+            K = 2 * N() - 1;
+        v.push({
+            x: G,
+            y: H,
+            xa: y,
+            ya: K,
+            max: 6000
+        });
+    }
+    setTimeout(function() {
+        M()
+    }, 100)
+} ();
 
+
+//nav  点击事件
+(function () {
+  // $(".nav li.skill").on("click",function () {
+  //   $(".moudle.skill").addClass("active");
+  //
+  // })
+  $(".nav li").on("click",function () {
+    var classname =  this.className;
+    $(".moudle")
+    .filter(function () {
+      return $(this).hasClass(classname)
+    })
+      .addClass("active")
+    .siblings(".moudle")
+      .removeClass("active")
   })
+
+
+
 
 
 })();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});//DOMContentLoaded
+(function () {
+    
+    $(".moudle .container .inner").perfectScrollbar();
+    
+})();
